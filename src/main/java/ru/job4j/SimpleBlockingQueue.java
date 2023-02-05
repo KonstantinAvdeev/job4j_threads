768b3b37754e2a0eb4d2190123041bea5ef54fc6
Konstantin Avdeev <fujiun91@gmail.com>
2023-02-05 15:46:38 +0300

    1. Реализовать шаблон Producer Consumer. [#1098]

---
+++ src/main/java/ru/job4j/SimpleBlockingQueue.java
@@ -0,0 +1,50 @@
+package ru.job4j;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+    private int limit;
+
+    public SimpleBlockingQueue(int limit) {
+        this.limit = limit;
+    }
+
+    public void offer(T value) {
+        synchronized (this) {
+            while (queue.size() == limit) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            queue.offer(value);
+            notifyAll();
+        }
+    }
+
+    public T poll() {
+        synchronized (this) {
+            while (queue.isEmpty()) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            T t = queue.peek();
+            queue.remove();
+            notifyAll();
+            return t;
+        }
+    }
+
+}
---
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -0,0 +1,39 @@
+package ru.job4j;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@ThreadSafe
+public class SimpleBlockingQueueTest {
+    @GuardedBy("this")
+    SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(8);
+    List<Integer> list = new LinkedList<>();
+
+    @Test
+    public void whenProduce() throws InterruptedException {
+        Thread producer = new Thread(() -> {
+            for (int i = 0; i < 8; i++) {
+                queue.offer(i);
+            }
+        });
+        Thread consumer = new Thread(() -> {
+            for (int i = 8; i > 0; i--) {
+                list.add(queue.poll());
+            }
+        });
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+        assertThat(list.get(7), is(7));
+        assertThat(list.get(5), is(5));
+    }
+
+}
