2d0d046c7f55eb34d1797cc238929db4a20266bc
Konstantin Avdeev <fujiun91@gmail.com>
2023-02-19 18:52:02 +0300

    3. ForkJoinPool [#315067]fix

--- src/test/java/ru/job4j/pools/ParallelArraySearchTest.java
+++ src/test/java/ru/job4j/pools/ParallelArraySearchTest.java
@@ -13,13 +13,13 @@ public class ParallelArraySearchTest {
 
     @Test
     public void whenParallelSearch() {
-        assertThat(ParallelArraySearch.search(new Object[]{5, 2423, 456, 24, 66, 452, 1, 14253, 23423412, 425, 6, 23, 252, 345, 14324}
-                , 1)).isEqualTo(6);
+        assertThat(ParallelArraySearch.search(new Object[]{5, 2423, 456, 24, 66, 452, 1, 14253, 23423412, 425, 6, 23, 252, 345, 14324},
+                1)).isEqualTo(6);
     }
 
     @Test
     public void whenElementNotFound() {
-        assertThat(ParallelArraySearch.search(new Object[]{5, 2423, 456, 24, 66, 452, 1, 14253, 23423412, 425, 6, 23, 252, 345, 14324}
-                , 1.0)).isEqualTo(-1);
+        assertThat(ParallelArraySearch.search(new Object[]{5, 2423, 456, 24, 66, 452, 1, 14253, 23423412, 425, 6, 23, 252, 345, 14324},
+                1.0)).isEqualTo(-1);
     }
 }
